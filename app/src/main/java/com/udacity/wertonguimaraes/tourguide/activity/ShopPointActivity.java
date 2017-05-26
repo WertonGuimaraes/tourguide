0147bb728cf63a4f6d9f903d5364466281b418b8
Werton Guimarães <werton.gomes@ccc.ufcg.edu.br>
2017-05-26 11:17:10 -0300

    atualização de dados de shoppings

--- app/src/main/java/com/udacity/wertonguimaraes/tourguide/activity/ShopPointActivity.java
+++ app/src/main/java/com/udacity/wertonguimaraes/tourguide/activity/ShopPointActivity.java
@@ -21,12 +21,31 @@ public class ShopPointActivity extends InfoActivity {
         List<Info> mInfoList = new ArrayList<>();
         mInfoList.add(
                 new Info(
+                        1,
+                        "VILA DO ARTESÃO",
+                        "Avenida Almeida Barreto, s/n - São José, Campina Grande - PB",
+                        "(83) 3322-2425",
+                        "Local muito propicio para quem quer comprar coisas regionais. Local tem uma praca de alimentação, lojas para compras. Loja de roupas, sapatos e artesanato. Ambiente lindo e pessoas agradaveis.",
+                        R.drawable.shop_sao_joao));
+
+        mInfoList.add(
+                new Info(
+                        2,
+                        "PARTAGE SHOPPING",
+                        "Avenida Severino Bezerra Cabral, 1050 - Catolé, Campina Grande - PB",
+                        "(83) 3344-5000",
+                        "O Partage é o principal centro de compras, gastronomia, serviços e entretenimento da região.",
+                        R.drawable.shop_partage));
+
+        mInfoList.add(
+                new Info(
                         3,
-                        "FARRA DE BODEGA",
+                        "LUIZA MOTTA",
                         "R. Afrigio Ribeiro Brito, 24 - Jardim Paulistano, Campina Grande - PB",
-                        null,
-                        "O monumento Farra de Bodega homenageia dois ícones da música nordestina: Jackson do Pandeiro e Luiz Gonzaga.",
-                        R.drawable.banner_luis_e_jackson));
+                        "(83) 3337-2627",
+                        "É um shopping pequeno, mas interessante. Boa praça de alimentação. No período junino, nos finais de semana tem shows a tarde na praça de alimentação. Também bom para comprar.",
+                        R.drawable.shop_luiza_motta)
+        );
 
         return mInfoList;
     }
